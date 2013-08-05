e7fedb16e647f8f8ba43e24e0553acb7dedb311a
Philip Holleran <pholleran@esri.com>
2013-08-05 14:09:51 -0500

    Update layout.js
    disabled update server/service (needs fixing for bugs)

--- js/layout.js
+++ js/layout.js
@@ -131,13 +131,14 @@ require([
         });
         taskMenu.addChild(shortUrlItem);
         
+        /*
         var serverNameItem = new MenuItem({
         	id: "serverNameItem",
         	label: "Update Server/Service Path",
         	onClick: function(){app.makeTab("serverNameTab","Update Server/Service Path","./custom/templates/updateServerNameTab.html")},
         });
         taskMenu.addChild(serverNameItem);
-
+	*/
         /*
         var eMailItem = new MenuItem({
         	id: "eMailItem",
